953f0dc54f61b8ce2889154e4b8906f8a337f010
Nuno Fachada <faken@fakenmc.com>
2014-11-15 13:39:06 +0000

    Add RNG stub test

---
+++ src/tests/test_rng.c
@@ -0,0 +1,56 @@
+/*
+ * This file is part of cf4ocl (C Framework for OpenCL).
+ *
+ * cf4ocl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * cf4ocl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with cf4ocl. If not, see <http://www.gnu.org/licenses/>.
+ * */
+
+/**
+ * @file
+ * Test the RNG class.
+ *
+ * @author Nuno Fachada
+ * @date 2014
+ * @copyright [GNU General Public License version 3 (GPLv3)](http://www.gnu.org/licenses/gpl.html)
+ * */
+
+#include <cl_ops.h>
+
+/**
+ * Tests sampler wrapper class reference counting.
+ * */
+static void impl_test() {
+
+	return;
+
+}
+
+/**
+ * Main function.
+ * @param[in] argc Number of command line arguments.
+ * @param[in] argv Command line arguments.
+ * @return Result of test run.
+ * */
+int main(int argc, char** argv) {
+
+	g_test_init(&argc, &argv, NULL);
+
+	g_test_add_func(
+		"/rng/impl",
+		impl_test);
+
+	return g_test_run();
+}
+
+
+
